6086596d30a53938e1b05433469ab50ff6c20fe2
sauron <1369130123@qq.com>
2019-08-06 10:48:13 +0800

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -69,10 +69,10 @@ local_ip = get_host_ip()
 myMQTTClient = AWSIoTMQTTClient("pynq_greengrass_sensor")
 
 #Here to put your own endpoint
-myMQTTClient.configureEndpoint("ai57r4nger0el-ats.iot.us-east-2.amazonaws.com", 443)
+myMQTTClient.configureEndpoint("xxxx.com", 443)
 
 #Here to put your own files
-myMQTTClient.configureCredentials("root-ca-cert.pem", "1fc3a2841c.private.key", "1fc3a2841c.cert.pem")
+myMQTTClient.configureCredentials("root-ca-cert.pem", "xxxx.private.key", "xxxx.cert.pem")
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
