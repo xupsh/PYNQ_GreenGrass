2d77cad0d78951d524185e907535efc8b4d06cdc
sauron <1369130123@qq.com>
2019-08-09 16:35:58 +0800

    Update button.py

--- button.py
+++ button.py
@@ -72,8 +72,8 @@ while 1:
     print(payload,'degree Celsius')
     #payload = '{ "ip_source: "'+ local_ip +'","timestamp": "' + now_str + '","instruction": ' + btn + ' }'
     #payload = btn
-    if btn != '0':
-        myMQTTClient.publish("hello/world/pubsub", payload, 0)
+    #if btn != '0':
+    myMQTTClient.publish("hello/world/pubsub", payload, 0)
     sleep(0.2)
 
 
